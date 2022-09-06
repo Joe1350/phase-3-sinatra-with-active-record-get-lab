009bf159814faab6754078cd54818642d263db57
Joseph Martin <josephmartin1350@gmail.com>
2022-09-05 21:16:45 -0600

    commit tree 3e133180d213582ecbd224d8db0cf2cfa81f0830

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,30 @@
 class ApplicationController < Sinatra::Base
 
+  set :default_content_type, 'application/json'
+
   # add routes
+  get '/' do
+    '<h2>error</h2>'
+  end
+
+  get '/bakeries' do
+    bakery = Bakery.all
+    bakery.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    goods = BakedGood.all.order(price: :desc)
+    goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    goods = BakedGood.all.order(price: :desc).first
+    goods.to_json
+  end
 
 end
